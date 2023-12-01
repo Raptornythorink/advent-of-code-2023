552f310a6c864d18ad3368608bebce76174c70e7
Aymeric Chaumont <aymeric.chaumont@student-cd.fr>
2023-12-01 19:32:07 +0100

    day 01

---
+++ 01/01-1.py
@@ -0,0 +1,11 @@
+def main():
+    with open("./input", "r") as file:
+        total = 0
+        for line in file:
+            digits = list(filter(lambda x: x.isdigit(), line))
+            if digits:
+                total += 10 * int(digits[0]) + int(digits[-1])
+    print(total)
+
+if __name__ == "__main__":
+    main()
