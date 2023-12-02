6d11c5b23afa30cf6bfdad18b86b5cc455d1313d
Aymeric Chaumont <aymeric.chaumont@student-cd.fr>
2023-12-02 12:28:33 +0100

    day 02

---
+++ 02/02-1.py
@@ -0,0 +1,30 @@
+def valid_combination(number: int, color: str) -> bool:
+    if color == "red" and number > 12:
+        return False
+    if color == "green" and number > 13:
+        return False
+    if color == "blue" and number > 14:
+        return False
+    return True
+
+def main():
+    possible_games = []
+    with open("./input", "r") as file:
+        for i, line in enumerate(file):
+            possible = True
+            game_inputs = line.strip().split(": ")[1].split("; ")
+            for game_input in game_inputs:
+                round_inputs = game_input.split(", ")
+                for round_input in round_inputs:
+                    number, color = round_input.split(" ")
+                    if not valid_combination(int(number), color):
+                        possible = False
+                        break
+                if not possible:
+                    break
+            if possible:
+                possible_games.append(i + 1)
+    print(sum(possible_games))
+
+if __name__ == "__main__":
+    main()
---
+++ 02/02-2.py
@@ -0,0 +1,23 @@
+def main():
+    powers = []
+    with open("./input", "r") as file:
+        for line in file:
+            game_inputs = line.strip().split(": ")[1].split("; ")
+            min_red = 0
+            min_green = 0
+            min_blue = 0
+            for game_input in game_inputs:
+                round_inputs = game_input.split(", ")
+                for round_input in round_inputs:
+                    number, color = round_input.split(" ")
+                    if color == "red":
+                        min_red = max(min_red, int(number))
+                    elif color == "green":
+                        min_green = max(min_green, int(number))
+                    elif color == "blue":
+                        min_blue = max(min_blue, int(number))
+            powers.append(min_red * min_green * min_blue)
+    print(sum(powers))
+
+if __name__ == "__main__":
+    main()
