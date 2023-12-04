c42341571c12a98717c27a07dad980241312fedd
Aymeric Chaumont <aymeric.chaumont@student-cd.fr>
2023-12-04 19:22:56 +0100

    day 04

---
+++ 04/04-1.py
@@ -0,0 +1,18 @@
+def main():
+    total_points = 0
+
+    with open("./input", "r") as file:
+        for line in file:
+            matches = 0
+            winning_numbers, numbers = map(lambda x: x.split(), line.strip().split(": ")[1].split("|"))
+            winning_numbers = set(winning_numbers)
+            for number in numbers:
+                if number in winning_numbers:
+                    matches += 1
+            if matches >= 1:
+                total_points += 2 ** (matches - 1)
+
+    print(total_points)
+
+if __name__ == "__main__":
+    main()
---
+++ 04/04-2.py
@@ -0,0 +1,26 @@
+def main():
+    won_cards = {}
+
+    with open("./input", "r") as file:
+        for i, line in enumerate(file):
+            matches = 0
+            winning_numbers, numbers = map(lambda x: x.split(), line.strip().split(": ")[1].split("|"))
+            winning_numbers = set(winning_numbers)
+            for number in numbers:
+                if number in winning_numbers:
+                    matches += 1
+            won_cards[i] = range(i + 1, i + 1 + matches)
+    card_number = i
+
+    points = {i: 1 for i in range(card_number + 1)}
+    
+    for i in range(card_number, -1, -1):
+        if i in won_cards:
+            for j in won_cards[i]:
+                if j in points:
+                    points[i] += points[j]
+    
+    print(sum(points.values()))
+
+if __name__ == "__main__":
+    main()
